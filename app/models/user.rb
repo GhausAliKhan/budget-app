adc89a1112e3a8c30436c3142131242ce4873d1e
GhausAliKhan <ghauskhan1616@gmail.com>
2023-12-19 12:20:18 +0500

    Add associations and validations to user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_many :expenses, foreign_key: :author_id, dependent: :destroy
+  has_many :categories, dependent: :destroy
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
 end

a522cc57d98d08b55f967d92f4282915621acc29
GhausAliKhan <ghauskhan1616@gmail.com>
2023-12-19 16:11:41 +0500

    Fix Linters: Empty block detected

--- spec/requests/categories_spec.rb
+++ spec/requests/categories_spec.rb
@@ -1,4 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe 'Categories', type: :request do
+  describe 'GET /index' do
+    it 'returns http success' do
+      get '/categories/index'
+    end
+  end
 end
--- spec/requests/expenses_spec.rb
+++ spec/requests/expenses_spec.rb
@@ -1,4 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe 'Expenses', type: :request do
+  describe 'GET /new' do
+    it 'returns http success' do
+      get '/expenses/new'
+    end
+  end
 end

2b1cf2277b2532a66533ddafadeb6874c8a5f84c
GhausAliKhan <ghauskhan1616@gmail.com>
2023-12-21 15:01:17 +0500

    Add --trace to get detailed trace of the build error

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,6 +2,6 @@
 set -o errexit
 
 bundle install
-bundle exec rake assets:precompile
-bundle exec rake assets:clean
-bundle exec rake db:migrate
+bundle exec rake --trace assets:precompile
+bundle exec rake --trace assets:clean
+bundle exec rake --trace db:migrate
